edc569ee65f69742691ffb46641a06ba5b07b267
Aibyilann <aihomestudio680@gmail.com>
2026-01-16 15:20:11 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// static files
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "about.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "contact.html"));
+});
+
+// ⛔ JANGAN app.listen
+module.exports = app;
